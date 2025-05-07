afa99c32f0612ba2ffb0fd14a96dffb8be37c04e
Vytenis <vytenis.triuska@gmail.com>
2025-05-07 19:08:53 +0300

    Converted the project to Spring Boot.

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -1,8 +1,12 @@
 package project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello Tax Free");
+        SpringApplication.run(App.class, args);
     }
 }
